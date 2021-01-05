07a3c31fda351a2b1d56dea04c3d6ad83694ba1e
Red_M <git@red-m.net>
2021-01-06 00:44:17 +1000

    Minor changes to builds for wheels.

--- ci/gitlab/build-wheels.sh
+++ ci/gitlab/build-wheels.sh
@@ -7,17 +7,17 @@ cd /io
 
 # Compile wheels
 for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
-    "${PYBIN}/pip" wheel /io/wheelhouse/*.gz -w wheelhouse/
+    "${PYBIN}/pip" wheel /io/wheelhouse/*.gz -w /io/wheelhouse/
 done
 cd "${OLD_PWD}"
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /io/wheelhouse/
+    auditwheel repair "${whl}" -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
     "${PYBIN}/pip" install redlibssh2 --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/python" -c 'import ssh2; ssh2.session.Session(); print(ssh2.__version__)')
+    (cd "${HOME}"; "${PYBIN}/python" -c 'import ssh2; ssh2.session.Session(); print(ssh2.__version__)')
 done
