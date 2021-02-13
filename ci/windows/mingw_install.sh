683e7021b8fdc4839eebcf6d5190b5408ae7cad2
Red_M <git@red-m.net>
2021-02-13 16:58:20 +1000

    Add zlib for Windows builds.

--- ci/windows/mingw_install.sh
+++ ci/windows/mingw_install.sh
@@ -8,4 +8,8 @@ cd ~/mingw
 TARGET_PKG=mingw-w64-x86_64-openssl-1.1.1.i-1-any.pkg.tar.zst
 wget "https://repo.msys2.org/mingw/x86_64/${TARGET_PKG}"
 zstd -d "./${TARGET_PKG}"
+TARGET_PKG=mingw-w64-x86_64-zlib-1.2.11-9-any.pkg.tar.zst
+wget "https://repo.msys2.org/mingw/x86_64/${TARGET_PKG}"
+zstd -d "./${TARGET_PKG}"
+tar xvf *.tar
 cd "${OLD_PATH}"
