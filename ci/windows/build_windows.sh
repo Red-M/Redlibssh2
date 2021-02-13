9cd098a16ea96392388b9e44a1bfe9e4cb3543e4
Red_M <git@red-m.net>
2021-02-13 16:16:26 +1000

    Tweaks from testing Windows builds.

--- ci/windows/build_windows.sh
+++ ci/windows/build_windows.sh
@@ -3,6 +3,6 @@
 export DOCKER_TAG=red_m/redlibssh2
 rm -rf ./build ./dist ./ssh2/libssh2.* ./libssh2/.git
 
-docker build -t $DOCKER_TAG -f ci/docker/manylinux_2010/Dockerfile .
+docker build -t $DOCKER_TAG -f ci/windows/Dockerfile .
 docker run --rm -v `pwd`:/io $DOCKER_TAG /io/ci/gitlab/build_windows_wheels.sh
 ls wheelhouse/
---
+++ ci/windows/mingw_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum groupinstall -y "MinGW cross-compiler"
+yum install -y zstd wget
+mkdir -p ~/mingw
+OLD_PATH=$(pwd)
+cd ~/mingw
+TARGET_PKG=mingw-w64-x86_64-openssl-1.1.1.i-1-any.pkg.tar.zst
+wget "https://repo.msys2.org/mingw/x86_64/${TARGET_PKG}"
+zstd -d "./${TARGET_PKG}"
+cd "${OLD_PATH}"
