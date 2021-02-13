8278668edc5683e8356774774b7d318fc37bcdf3
Red_M <git@red-m.net>
2021-02-13 17:11:51 +1000

    Fix tar extraction for mingw libs.

--- ci/windows/mingw_install.sh
+++ ci/windows/mingw_install.sh
@@ -8,8 +8,9 @@ cd ~/mingw
 TARGET_PKG=mingw-w64-x86_64-openssl-1.1.1.i-1-any.pkg.tar.zst
 wget "https://repo.msys2.org/mingw/x86_64/${TARGET_PKG}"
 zstd -d "./${TARGET_PKG}"
+tar xvf mingw-w64-x86_64-openssl-*.tar
 TARGET_PKG=mingw-w64-x86_64-zlib-1.2.11-9-any.pkg.tar.zst
 wget "https://repo.msys2.org/mingw/x86_64/${TARGET_PKG}"
 zstd -d "./${TARGET_PKG}"
-tar xvf *.tar
+tar xvf mingw-w64-x86_64-zlib-*.tar
 cd "${OLD_PATH}"
