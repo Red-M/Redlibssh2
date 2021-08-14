a4fa764da73c12365f363356a1384c54e8a94b7f
Red_M <git@red-m.net>
2021-08-15 04:02:50 +1000

    Testing of OSX multi-version builds

--- ci/travis/pyenv-wheel.sh
+++ ci/travis/pyenv-wheel.sh
@@ -4,31 +4,35 @@ brew install pyenv || brew outdated pyenv || brew upgrade pyenv
 
 export BUILD_PYENV_VERSIONS=("${PYENV:-ALL}")
 ALL_PYENV_VERSION_DEFAULT=("ALL")
-ALL_BUILD_PYENV_VERSIONS=("3.9.6" "3.8.11" "3.7.11" "3.6.14" "3.5.10")
+# ALL_BUILD_PYENV_VERSIONS=("3.9.6" "3.8.11" "3.7.11" "3.6.14" "3.5.10")
+ALL_BUILD_PYENV_VERSIONS=("3.9.6" "3.8.11" "3.7.11")
 
 if [ ${BUILD_PYENV_VERSIONS[@]} = ${ALL_PYENV_VERSION_DEFAULT[@]} ]; then
     BUILD_PYENV_VERSIONS=(${ALL_BUILD_PYENV_VERSIONS[@]})
 fi
 
+set +x
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+set -x
+if [[ ! -d "./venv" ]]; then
+    mkdir ./venv
+fi
+
 for PYENV in ${BUILD_PYENV_VERSIONS[@]}; do
-    export PYENV_VERSION=$PYENV
     if [[ ! -d "$HOME/.pyenv/versions/$PYENV" ]]; then
         pyenv install $PYENV
     fi
+
     pyenv shell $PYENV
     pyenv versions
 
-    set +x
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-    set -x
-
     which python
     python -m pip install -U virtualenv
-    python -m virtualenv -p "$(\which python)" venv
+    python -m virtualenv -p "${PYENV_VERSION}" "venv/${PYENV_VERSION}"
 
     set +x
-    source venv/bin/activate
+    source "venv/${PYENV_VERSION}/bin/activate"
     set -x
 
     python -V
@@ -44,7 +48,7 @@ for PYENV in ${BUILD_PYENV_VERSIONS[@]}; do
     rm -f *.dylib
     pip install -v ./*.whl
     mkdir -p temp; cd temp
-    python -c "from ssh2.session import Session; Session()" && echo "Import successfull"
+    python -c "from ssh2.session import Session; Session()" && echo "Import successful"
     cd ..
     set +x
     deactivate
