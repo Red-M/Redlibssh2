5e8b6addda0764a8ad14b639cd3b6c96ff00468f
Red_M <git@red-m.net>
2021-09-02 01:32:06 +1000

    Add builds for pypy and manylinux_2

---
+++ ci/gitlab/linux/build_manylinux_pypy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export DOCKER_TAG=red_m/redlibssh2
+rm -rf ./build ./dist ./ssh2/libssh2.* ./libssh2/.git
+
+docker build -t $DOCKER_TAG -f ci/gitlab/linux/Dockerfile_manylinux_pypy .
+docker run --rm -v `pwd`:/io $DOCKER_TAG /io/ci/gitlab/linux/build_wheels.sh pypy pypy
+ls wheelhouse/
--- ci/gitlab/linux/build_wheels.sh
+++ ci/gitlab/linux/build_wheels.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -xe
 
+PYTHON_DIR=${1:-python}
+PYTHON_BIN=${2:-python}
+
 OLD_PWD="$(pwd)"
-LATEST_PY="$(ls -1d /opt/python/*/bin | grep -v cpython | tail -n1)/python"
+LATEST_PY="$(ls -1d /opt/${PYTHON_DIR}/*/bin | grep -v cpython | tail -n1)/${PYTHON_BIN}"
 cd /io
 "${LATEST_PY}" /io/setup.py sdist -d /io/wheelhouse --formats=gztar
 
 # Compile wheels
-for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
+for PYBIN in `ls -1d /opt/${PYTHON_DIR}/*/bin | grep -v cpython`; do
     "${PYBIN}/pip" wheel /io/wheelhouse/*.gz -w /io/wheelhouse/
 done
 cd "${OLD_PWD}"
@@ -18,7 +21,7 @@ for whl in /io/wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in `ls -1d /opt/python/*/bin | grep -v cpython`; do
+for PYBIN in `ls -1d /opt/${PYTHON_DIR}/*/bin | grep -v cpython`; do
     "${PYBIN}/pip" install redlibssh2 --no-index -f /io/wheelhouse
-    (cd "${HOME}"; "${PYBIN}/python" -c 'import ssh2; ssh2.session.Session(); print(ssh2.__version__)')
+    (cd "${HOME}"; "${PYBIN}/${PYTHON_BIN}" -c 'import ssh2; ssh2.session.Session(); print(ssh2.__version__)')
 done
