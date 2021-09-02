52a86e5e1d9d50c58a84779b07ed5a759f520b24
Red_M <git@red-m.net>
2021-09-02 17:15:23 +1000

    Fix bug in building manylinux wheels

--- ci/gitlab/linux/build_manylinux.sh
+++ ci/gitlab/linux/build_manylinux.sh
@@ -8,6 +8,6 @@ rm -rf ./build ./dist ./ssh2/libssh2.* ./libssh2/.git
 cp ./ci/gitlab/linux/Dockerfile_manylinux /tmp/
 sed -i 's#MANYLINUX_DOCKER_CONTAINER#'"${DOCKER_CONTAINER}"'#g' /tmp/Dockerfile_manylinux
 
-docker build -t $DOCKER_TAG -f ci/gitlab/linux/Dockerfile_manylinux .
+docker build -t $DOCKER_TAG -f /tmp/Dockerfile_manylinux .
 docker run --rm -v `pwd`:/io $DOCKER_TAG /io/ci/gitlab/linux/build_wheels.sh
 ls wheelhouse/
