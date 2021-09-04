27ca04473808fb9d60405acd385508e6a419c095
Red_M <git@red-m.net>
2021-09-04 14:52:00 +1000

    Fix debtesting tests

--- ci/gitlab/linux/install_deb_mirror.sh
+++ ci/gitlab/linux/install_deb_mirror.sh
@@ -3,6 +3,9 @@
 
 if [ ! -z $(getent hosts deb-mirror | awk '{ print $1 }') ]; then
     VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed -E 's/.+?\=//g')
+    if [ -z $VERSION_CODENAME ]; then
+        VERSION_CODENAME=$(grep deb.debian.org /etc/apt/sources.list | sed -E 's#.+?/debian (.+?) main$#\1#g')
+    fi
     echo "deb http://deb-mirror/deb.debian.org/debian ${VERSION_CODENAME} main non-free contrib
 deb-src http://deb-mirror/deb.debian.org/debian ${VERSION_CODENAME} main non-free contrib
 deb http://deb-mirror/deb.debian.org/debian ${VERSION_CODENAME}-updates main contrib non-free
