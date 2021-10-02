fadd5d484046a1decaef1c3f68814f1847ae32e8
Red_M <git@red-m.net>
2021-10-02 20:45:08 +1000

    Add aarch64 for builds

--- ci/gitlab/linux/setup_manylinux.sh
+++ ci/gitlab/linux/setup_manylinux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum -y --disablerepo="epel" install zlib-devel openssl-devel cmake gcc || true
+yum -y --disablerepo="epel" install zlib-devel openssl-devel cmake gcc || yum -y install zlib-devel openssl-devel cmake gcc || true
 apt && /scripts/setup_manylinux.sh || true
 apt update && apt install -y cmake make libssl-dev zlib1g-dev || true
