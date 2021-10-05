5a66c9394e4f264e7f0c094f86241863e37a38fe
Red_M <git@red-m.net>
2021-10-06 03:52:55 +1000

    Add additional MacOS CI builds

--- ci/travis/pyenv-wheel.sh
+++ ci/travis/pyenv-wheel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -xe
 
 export BUILD_PYENV_VERSIONS=("${PYENV:-ALL}")
+export PYENV_ROOT="${HOME}/${MACOSX_DEPLOYMENT_TARGET}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
 ALL_PYENV_VERSION_DEFAULT=("ALL")
-# ALL_BUILD_PYENV_VERSIONS=("3.9.6" "3.8.11" "3.7.11" "3.6.14" "3.5.10")
-ALL_BUILD_PYENV_VERSIONS=("3.9.6" "3.8.11" "3.7.11")
+ALL_BUILD_PYENV_VERSIONS=("3.9.7" "3.8.12" "3.7.12" "3.6.14" "3.5.10" "2.7.18")
+# ALL_BUILD_PYENV_VERSIONS=("3.9.7" "3.8.12" "3.7.12" "3.6.14" "3.5.10" "2.7.18" "pypy3.7-7.3.5" "pypy3.6-7.3.3" "pypy2.7-7.3.1")
+# ALL_BUILD_PYENV_VERSIONS=("3.9.7" "3.8.13" "3.7.13")
 
 if [ ${BUILD_PYENV_VERSIONS[@]} = ${ALL_PYENV_VERSION_DEFAULT[@]} ]; then
     BUILD_PYENV_VERSIONS=(${ALL_BUILD_PYENV_VERSIONS[@]})
@@ -18,8 +21,12 @@ if [[ ! -d "./venv" ]]; then
 fi
 
 for PYENV in ${BUILD_PYENV_VERSIONS[@]}; do
-    if [[ ! -d "$HOME/.pyenv/versions/$PYENV" ]]; then
-        pyenv install $PYENV
+    if [[ ! -d "${PYENV_ROOT}/versions/$PYENV" ]]; then
+        if [[ "$(uname)" == "Darwin" ]] && [[ $PYENV == 3.6* || $PYENV == 3.5* || $PYENV == 2.7* ]]; then
+            pyenv install --patch $PYENV < <(curl -sSL 'https://github.com/python/cpython/commit/8ea6353.patch?full_index=1')
+        else
+            pyenv install $PYENV
+        fi
     fi
 
     pyenv shell $PYENV
@@ -37,7 +44,7 @@ for PYENV in ${BUILD_PYENV_VERSIONS[@]}; do
     python -m pip install -U setuptools pip
     pip install -U delocate wheel
     pip wheel .
-    \cp /usr/local/lib/libssh2* .
+    # \cp /usr/local/lib/libssh2* .
     delocate-listdeps --all ./*.whl
     delocate-wheel -v ./*.whl
     delocate-listdeps --all ./*.whl
