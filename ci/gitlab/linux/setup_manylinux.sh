a09c7c023836253d080d89bfb7a808043baa4969
Red_M <git@red-m.net>
2022-04-11 19:47:55 +1000

    Add local deb mirror for package updates and upgrade packages

--- ci/gitlab/linux/setup_manylinux.sh
+++ ci/gitlab/linux/setup_manylinux.sh
@@ -2,5 +2,5 @@
 
 yum -y --disablerepo="epel" install zlib-devel openssl-devel cmake gcc || yum -y install zlib-devel openssl-devel cmake gcc || true
 yum -y --disablerepo="epel" update || yum -y update || true
-apt && /scripts/install_deb_mirror.sh || true
+apt update && /scripts/install_deb_mirror.sh || true
 apt update && apt install -y cmake make libssl-dev zlib1g-dev build-essential && apt upgrade -y || true
